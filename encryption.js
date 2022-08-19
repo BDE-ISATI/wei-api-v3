778ed98f3e41a1eba3a4a7377f171db18c29e299
Théo Le Goc <theo@legoc.net>
2022-08-19 15:58:22 +0200

    Wrong way of exporting function, thanks javascript

--- encryption.js
+++ encryption.js
@@ -1,14 +1,19 @@
-export const currentKeyPairs = [];
+const currentKeyPairs = [];
 
-export function generateKeyPairs() {
-    const settings = {
-        name: "RSA-PSS",
-        modulusLength: 4096,
-        publicExponent: [0x01, 0x00, 0x01],
-        hash: "SHA-256"
-    }
+const settings = {
+    name: "RSA-PSS",
+    modulusLength: 4096,
+    publicExponent: [0x01, 0x00, 0x01],
+    hash: "SHA-256"
+}
 
+function generateKeyPairs() {
     SubtleCrypto.generateKey(settings, false, ["encrypt", "decrypt"]).then(keyPair => {
         console.log(keyPair);
     });
+}
+
+module.exports = {
+    generateKeyPairs, 
+    currentKeyPairs
 }
